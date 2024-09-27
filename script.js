feb21a9f0871a98a7971f15d8776b366901a442b
Sai-2019 <157068149+Sai-2019@users.noreply.github.com>
2024-09-27 05:02:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById("viewProjectsBtn").addEventListener("click", function() {
-    document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
-  });
-  
-  document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Your message has been sent. Thank you!");
-  });
-  
+
